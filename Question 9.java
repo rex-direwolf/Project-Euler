9b6c5146926b4e0e5bf3d16b5c1206e011c435a6
Sourabh Kumar Singh <53351336+rex-direwolf@users.noreply.github.com>
2019-08-04 16:55:48 +0530

    Add files via upload

---
+++ Question 9.java
@@ -0,0 +1,21 @@
+  class P9
+{
+	public static void main(String args[])
+	{
+		
+		int i,j,k=0;
+		for(i=1;i<=1000;i++)
+		{
+			for(j=i+1;j<1000;j++)
+			{
+				
+				k=1000-i-j;
+				if((k*k)==(i*i)+(j*j))
+				{
+    System.out.println("Product of Pythagorean triplet is="+i*j*k);
+				 break;
+			 	}}
+			 	}
+			 	}
+			 	}
+			
